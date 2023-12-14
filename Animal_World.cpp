0bd45dfd92bd83694e49760e54e1f0487d187695
Arlikey <arkeyfast@gmail.com>
2023-12-14 19:28:00 +0200

    Added classes Eurasia, Elk and Tiger

--- Animal_World.cpp
+++ Animal_World.cpp
@@ -35,6 +35,13 @@ public:
 	}
 };
 
+class Elk : public Herbivore_Animal {
+public:
+	void SetWeight() override {
+		weight = rand() % (600 - 360) + 360;
+	}
+};
+
 class Carnivore_Animal abstract {
 protected:
 	int power;
@@ -69,6 +76,13 @@ public:
 	}
 };
 
+class Tiger : public Carnivore_Animal {
+public:
+	void SetPower() override {
+		power = rand() % (800 - 300) + 300;
+	}
+};
+
 class Continent abstract {
 public:
 	virtual Herbivore_Animal* CreateHerbivoreAnimal() abstract;
@@ -100,6 +114,21 @@ public:
 	}
 	Carnivore_Animal* CreateCarnivoreAnimal() {
 		Carnivore_Animal* animal = new Wolf();
+		animal->SetPower();
+		return animal;
+	}
+};
+
+class Eurasia : public Continent {
+public:
+	Herbivore_Animal* CreateHerbivoreAnimal() {
+		Herbivore_Animal* animal = new Elk();
+		animal->SetWeight();
+		animal->SetLife(true);
+		return animal;
+	}
+	Carnivore_Animal* CreateCarnivoreAnimal() {
+		Carnivore_Animal* animal = new Tiger();
 		animal->SetPower();
 		return animal;
 	}
@@ -140,12 +169,32 @@ int main() {
 	
 	Animal_World* world = new Animal_World(continent);
 
+	std::cout << "Africa" << std::endl;
 	world->Meals_Herbivores();
 	world->Nutrition_Carnivores();
 
 	delete continent;
 	delete world;
 
+	std::cout << "\nNorth America" << std::endl;
+	continent = new North_America();
+	world = new Animal_World(continent);
+
+	world->Meals_Herbivores();
+	world->Nutrition_Carnivores();
+
+	delete continent;
+	delete world;
+
+	std::cout << "\nEurasia" << std::endl;
+	continent = new Eurasia();
+	world = new Animal_World(continent);
+
+	world->Meals_Herbivores();
+	world->Nutrition_Carnivores();
+
+	delete continent;
+	delete world;
 
 	return 0;
 }
